ec0c1ea6fb00b80888c80305a5e013ceedcd65db
Shawn DelPercio <sdelperc@gmail.com>
2020-02-19 18:46:01 -0500

    added in calculation for distance and action for whether ISS is overhead

--- is-iss-overhead/src/components/Result.js
+++ is-iss-overhead/src/components/Result.js
@@ -1,13 +1,51 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { trueResult, falseResult, resetResult } from '../actions';
 
 const Result = props => {
+	// recalculate result
+	useEffect(
+		props => {
+			const lat1 = props.user_latlong.lat;
+			const lng1 = props.user_latlong.lng;
+			const lat2 = props.iss_latlong.latitude;
+			const lng2 = props.iss_latlong.longitude;
+
+			// converter
+			function deg2rad(deg) {
+				return deg * (Math.PI / 180);
+			}
+
+			function getDistanceBetween(lat1, lng1, lat2, lng2) {
+				var R = 6371; // radius of earth in km
+				var dLat = deg2rad(lat2 - lat1); // converting degrees to radians
+				var dLng = deg2rad(lng2 - lng1);
+				var a =
+					Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+					Math.cos(deg2rad(lat1)) *
+						Math.cos(deg2rad(lat2)) *
+						Math.sin(dLng / 2) *
+						Math.sin(dLng / 2);
+				var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+				var d = R * c;
+				return d;
+			}
+
+			const isIssOverhead = () => {
+				if (getDistanceBetween(lat1, lng1, lat2, lng2) < 100) {
+					props.trueResult();
+				} else {
+					props.falseResult();
+				}
+			};
+			isIssOverhead();
+		},
+		[props.isFetching]
+	);
+
 	return (
 		<>
-			{!props.searched ? (
-				<p>Enter in an address to see if the ISS is overhead!</p>
-			) : (
+			{props.searched && (
 				<div>
 					{props.result ? (
 						<p>
@@ -39,6 +77,5 @@ const mapStateToProps = state => {
 
 export default connect(mapStateToProps, {
 	trueResult,
-	falseResult,
-	resetResult
+	falseResult
 })(Result);
